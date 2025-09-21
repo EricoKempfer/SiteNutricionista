efadaafede41540250cd2681caa2e5ec3c45f96a
Érico <ericokempfer@gmail.com>
2025-09-21 17:43:51 -0300

    speed

--- my-app/src/pages/_app.js
+++ my-app/src/pages/_app.js
@@ -1,9 +1,11 @@
 import { Provider } from "@/components/ui/provider"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider >
       <Component {...pageProps} />
+      <SpeedInsights/>
     </Provider>
   )
 }
