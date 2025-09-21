4c1b86c471cdb42b182bfd7b50d583b6857d38b1
Érico <ericokempfer@gmail.com>
2025-09-21 17:44:59 -0300

    analytics

--- my-app/src/pages/_app.js
+++ my-app/src/pages/_app.js
@@ -1,11 +1,13 @@
 import { Provider } from "@/components/ui/provider"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider >
       <Component {...pageProps} />
       <SpeedInsights/>
+      <Analytics/>
     </Provider>
   )
 }
