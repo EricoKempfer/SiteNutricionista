e6bf278731212a678079ebed84bde3dc952d4cd2
erico <ericokempfer@gmail.com>
2025-09-23 18:00:32 -0300

    feat: Configurar Google Analytics (G-92GX47LHHY)
    - Adicionar Google tag (gtag.js) no _document.js
    - Configurar rastreamento de página no _app.js
    - Atualizar arquivo analytics.js com ID correto
    - Implementar rastreamento automático de mudanças de rota

--- my-app/src/pages/_app.js
+++ my-app/src/pages/_app.js
@@ -1,8 +1,11 @@
 import { Provider } from "@/components/ui/provider"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/next"
+import { useAnalytics } from "../lib/analytics"
 
 export default function App({ Component, pageProps }) {
+  // Hook para rastrear mudanças de página no Google Analytics
+  useAnalytics();
   return (
     <Provider >
       <Component {...pageProps} />
--- my-app/src/pages/_document.js
+++ my-app/src/pages/_document.js
@@ -18,6 +18,22 @@ export default function Document() {
         {/* PWA Manifest */}
         <link rel="manifest" href="/manifest.json" />
         
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-92GX47LHHY"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-92GX47LHHY');
+            `,
+          }}
+        />
+        
         {/* Schema.org JSON-LD para dados estruturados */}
         <script
           type="application/ld+json"
